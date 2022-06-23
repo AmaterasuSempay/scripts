0b65b6f945dab9de88df951481b543ea7ee393b4
AmaterasuSempay <105700115+AmaterasuSempay@users.noreply.github.com>
2022-06-23 09:44:29 +0300

    Update odoo-backup-script-log.sh

--- odoo-backup-script-log.sh
+++ odoo-backup-script-log.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-# vars
-BACKUP_DIR=/your/dir/
-SAVE_BASE=/your/dir1/
-ODOO_DATABASE=db1
-ADMIN_PASSWORD=your_admin_passwd
-LOG_DIR=/var/log/
-LOG_FILE=odoo-backup-sb.log
+# vars for config file
+. /pach/to/config_file.conf
+
+# activate service account
+gcloud auth activate-service-account ${SERVICE_ACCOUNT} --key-file=${ACCOUNT_KEY}
+
+# access secret from gcp secret mamager
+for secret in $(gcloud secrets versions access ${SECRET_VERSION} --secret=${SECRET_ID})
+do
+echo "$(date +%F.%T) secret received" >>  ${LOG_DIR}/${LOG_FILE}
+done
 
 # create a backup directory
 mkdir -p ${BACKUP_DIR}
 
 # create a backup
 curl -X POST \
-     -F "master_pwd=${ADMIN_PASSWORD}" \
+     -F "master_pwd=$secret" \
      -F "name=${ODOO_DATABASE}" \
      -F "backup_format=zip" \
      -o ${BACKUP_DIR}/${ODOO_DATABASE}.$(date +%F.%T).zip \
-     http://localhost:8069/web/database/backup
+     http://${DATABASE_IP}:${PORT}/web/database/backup
 
 # delete old backups
 find ${BACKUP_DIR} -type f -mtime 1 -name "${ODOO_DATABASE}.*.zip" -delete
+
+find ${SAVE_BASE} -type f -mtime 1 -name "${ODOO_DATABASE}.*.zip" -delete
 
 # copy backup to save base
 find ${BACKUP_DIR} -type f -cmin 1 -name "${ODOO_DATABASE}.*.zip" -exec cp '{}' ${SAVE_BASE} \;
